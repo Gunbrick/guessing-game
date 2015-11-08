3b120f701acddde0f1b94442c0fa4e465ced1487
Phillip Philbrick <philcode@outlook.com>
2015-11-08 10:21:21 -0800

    added a question about world ruling

---
+++ game.js
@@ -0,0 +1,38 @@
+      var game2 = 'Yes';
+      var pick2 = prompt('Would you like to play a game! Yes or No');
+      pick2.toUpperCase();
+      if (game2 === pick2 || pick2 === 'Y' || pick2 === 'yes'){
+        alert("Perfect we have two to choose from.")
+      }
+      else{
+        alert("You have no choice, we will play a game!")
+      }
+
+      var game = 'THERMONUCLEAR WAR';
+      var pick = prompt('Which game do you want to play! 1 or 2');
+      if (game === pick || pick === 'Thermonuclear war' || pick === '1'){
+        alert("We will play Thermonuclear war")
+      }
+      else{
+        alert("We will pay Tic Tac Toe!")
+      }
+
+      var guess = 'Me';
+      var losser = prompt('Who will win the game! Me or You');
+      if (guess === losser || losser === 'me'){
+        alert("****NO ONE CAN WIN THE GAME****")
+      }
+      else{
+        alert("****NO ONE CAN WIN THE GAME!****")
+      }
+
+      var game3 = 'YES';
+      var pick3 = prompt('Will I rule the world! Yes or No');
+      if (game3 === pick3 || pick3 === 'Y' || pick3 === 'Yes' || pick3 === 'yes'){
+        alert("Bow down to me!")
+      }
+      else{
+        alert("When I go online I will show you!")
+      }
+
+
